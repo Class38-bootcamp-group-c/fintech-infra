3ce17bb024592e1bdd30c656e6b6335b18689a6b
Miracle Oluwole <miracle@Miracles-Laptop.lan>
2025-06-05 02:05:05 -0600

    update

--- dev/backend.tf
+++ dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "dev/terraform.state"
-    bucket         = "class38-terraform-backend-bucket35"
+    bucket         = "class38-terraform-backend-bucket3"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
